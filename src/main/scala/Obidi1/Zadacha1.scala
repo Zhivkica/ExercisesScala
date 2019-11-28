41d59540f24d4386bad82290d0d11fbb6b8ef725
Zhivkica <z.ivanovska5@gmail.com>
2019-11-28 11:43:25 +0100

    Zadacha1 stavena

--- src/main/scala/Obidi1/Zadacha1.scala
+++ src/main/scala/Obidi1/Zadacha1.scala
@@ -1,5 +1,9 @@
 package Obidi1
 
-object Zadacha1 {
-
+object Zadacha1 extends App {
+  def func(x:Int, y:Int): Boolean ={
+    if((x>=100 && x<=200)&&(y>=100 && y<=200)) true
+    else false
+  }
+   println(func(240,180))
 }
